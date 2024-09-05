cdd89c93df5d5660bcf6f52a92ac7b077939a828
caioantnio <caashxw@gmail.com>
2024-09-05 17:35:55 -0300

    Aula Cadastro

--- consulta.php
+++ consulta.php
@@ -1,83 +1,55 @@
 <?php
-   include "conecta.php";
-?>
-<!-- 
-<!DOCTYPE html>
-<html lang=pt-br>
-<head>
+include "conecta.php";
 
-</head>
-<body>
-
-</body>
-
-</html>
-
-
-echo"<br><b>Mostrando todos os produtos</b><br>";
-
-$sql = "SELECT id, descricao, fabricante, quantidade, preco_custo, preco_venda, imagem FROM produtos";
+echo "<br><b>Mostrando todos os registros</b><br>";
+$sql = "SELECT id, nome, senha, email FROM usuario";
 $resultado = mysqli_query($conexao, $sql);
-
 if (mysqli_num_rows($resultado) > 0) {
     while ($registro = mysqli_fetch_assoc($resultado)) {
-        echo "id: " . $registro["id"]. " - Descrição: " . $registro["descricao"]. " - Fabricante " . $registro["fabricante"]. " - Quantidade" . $registro["quantidade"]. " - Custo: " . $registro["preco-custo"]. " - Preço: " . $registro["preco-venda"]. "<br>";
-    }
-} else {
-echo "Nenhum registro encontrado.";
-}
--->
-<?php
-   echo"<br><b>Mostrando todos os registros</b><br>";
-   $sql = "SELECT id, nome, senha, email FROM usuario";
-   $resultado = mysqli_query($conexao, $sql);
-   if (mysqli_num_rows($resultado) > 0) {
-    while ($registro = mysqli_fetch_assoc($resultado)) {
-        echo "<p style= 'background-Color:lightblue';> id: " . $registro["id"]. " - Nome: " . $registro["nome"].
-        " " . $registro["senha"]. " - Email: " . $registro["email"].
-        "</p>";
-    }
-} else {
-echo "Nenhum registro encontrado.";
-}
-
-   echo "<br><b>Nomes que começam com 'c'</b><br>";
-   $sql = "SELECT id, nome, senha, email FROM usuario WHERE nome LIKE 'c%';";
-   $resultado = mysqli_query($conexao, $sql);
-   echo"<br>";
-   if (mysqli_num_rows($resultado) > 0) {
-    while ($registro = mysqli_fetch_assoc($resultado)) {
-        echo "id: " . $registro["id"]. " - Nome: " . $registro["nome"].
-        " " . $registro["senha"]. " - Email: " . $registro["email"].
-        "<br>";
+        echo "<p style= 'background-Color:lightblue';> id: " . $registro["id"] . " - Nome: " . $registro["nome"] .
+            " " . $registro["senha"] . " - Email: " . $registro["email"] .
+            "</p>";
     }
 } else {
     echo "Nenhum registro encontrado.";
 }
 
-   echo "<br><b>Ordernar registros por email</b><br>";
-   $sql = "SELECT id, nome, senha, email FROM usuario order by email;";
-   $resultado = mysqli_query($conexao, $sql);
-   echo"<br>";
-   if (mysqli_num_rows($resultado) > 0) {
+echo "<br><b>Nomes que começam com 'c'</b><br>";
+$sql = "SELECT id, nome, senha, email FROM usuario WHERE nome LIKE 'c%';";
+$resultado = mysqli_query($conexao, $sql);
+echo "<br>";
+if (mysqli_num_rows($resultado) > 0) {
     while ($registro = mysqli_fetch_assoc($resultado)) {
-        echo "id: " . $registro["id"]. " - Nome: " . $registro["nome"].
-        " " . $registro["senha"]. " - Email: " . $registro["email"].
-        "<br>";
+        echo "id: " . $registro["id"] . " - Nome: " . $registro["nome"] .
+            " " . $registro["senha"] . " - Email: " . $registro["email"] .
+            "<br>";
     }
 } else {
     echo "Nenhum registro encontrado.";
 }
 
-   echo "<br><b>Verificar se existe usuário='Caju' e senha='123' e mostrar seu email. Caso não exista, mostrar uma mensagem.</b><br>";
-   $sql = "SELECT id, nome, senha, email FROM usuario where nome='Caju' and senha='123'";
-   $resultado = mysqli_query($conexao, $sql);
-   echo"<br>";
-   if (mysqli_num_rows($resultado) > 0) {
+echo "<br><b>Ordernar registros por email</b><br>";
+$sql = "SELECT id, nome, senha, email FROM usuario order by email;";
+$resultado = mysqli_query($conexao, $sql);
+echo "<br>";
+if (mysqli_num_rows($resultado) > 0) {
     while ($registro = mysqli_fetch_assoc($resultado)) {
-        echo "Email: " . $registro["email"]. "</p>";
+        echo "id: " . $registro["id"] . " - Nome: " . $registro["nome"] .
+            " " . $registro["senha"] . " - Email: " . $registro["email"] .
+            "<br>";
     }
 } else {
     echo "Nenhum registro encontrado.";
 }
-?>
+
+echo "<br><b>Verificar se existe usuário='Caju' e senha='123' e mostrar seu email. Caso não exista, mostrar uma mensagem.</b><br>";
+$sql = "SELECT id, nome, senha, email FROM usuario where nome='Caju' and senha='123'";
+$resultado = mysqli_query($conexao, $sql);
+echo "<br>";
+if (mysqli_num_rows($resultado) > 0) {
+    while ($registro = mysqli_fetch_assoc($resultado)) {
+        echo "Email: " . $registro["email"] . "</p>";
+    }
+} else {
+    echo "Nenhum registro encontrado.";
+}
